20641494ca3de1aa792c6c7cb5873de089b88631
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-04-02 14:31:55 +0000

    add socket

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 
 const express = require('express')
 const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const user = require('./routes/routes')
 const bodyParser = require('body-parser')
 const verifyToken = require('./app/middlewares/verifyToken')
 const fs = require('fs')
 
-port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 const dir = './backend/public/images'
@@ -23,11 +27,20 @@ app.use(express.static('public'));
 
 app.use('/api', user)
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.post('/api/post', verifyToken.verifyToken, (req, res) => {
-    res.send(req.authUser.dataUser)
-})
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
 
-app.listen(port, () => {
+// app.post('/api/post', verifyToken.verifyToken, (req, res) => {
+//     res.send(req.authUser.dataUser)
+// })
+
+server.listen(port, () => {
     console.log('API server started on: ' + port)
 })
