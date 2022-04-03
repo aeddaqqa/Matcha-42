dac3b25bf2771c0a6c59a37e1cb2c09ee02c1439
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-04-03 17:37:16 +0000

    edit friend check method

--- backend/app/Models/friend.js
+++ backend/app/Models/friend.js
@@ -2,8 +2,11 @@ const db = require("../../database/db")
 
 class Friend {
     check (id) {
-        let sql = "SELECT * FROM likes WHERE user_id1 = ? OR user_id2 = ? "
-        return db.promise().query(sql, [id, id])
+         const sql = '(SELECT users.id FROM users JOIN likes ON users.id = likes.user_id2 WHERE likes.user_id1 = ?)'
+         const sql2 = 'SELECT * FROM users JOIN likes ON users.id = likes.user_id1 WHERE likes.user_id2 = ? AND users.id'
+        const sql4 = sql2 + " IN " + sql
+        // const sql3 = sql2 + " INTERSECT " + sql
+        return db.promise().query(sql4, [id, id])
     }
 }
 
