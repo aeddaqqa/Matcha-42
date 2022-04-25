bb1d189141e7686f732c4495a1dda4d8bb5c73e4
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-04-25 01:18:10 +0000

    lin login ,register, verify email

--- client/src/views/CompleteProfile/VerifyEmail.js
+++ client/src/views/CompleteProfile/VerifyEmail.js
@@ -4,39 +4,38 @@ import axios from "axios";
 import MuiAlert from "@mui/material/Alert";
 
 const ComfirmAccount = () => {
-  const Alert = forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
-  const [searchParams] = useSearchParams();
-  const [message, setMessage] = useState({ error: null, success: null });
+    const Alert = forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+    const [searchParams] = useSearchParams();
+    const [message, setMessage] = useState({ error: null, success: null });
 
-  const postValidatEmail = async (token) => {
-    
-    axios
-      .post("http://localhost:1337/user/validate_email", { token })
-      .then((res) => {
-        setMessage({ ...message, success: res.data.message });
-        console.log(message);
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-        setMessage({ ...message, error: err.response.data.message });
-        console.log(message);
-      });
-  };
-  useEffect(() => {
-    const token = searchParams.get("token");
-    if (token) postValidatEmail(token);
-  }, []);
-  return (
-    <div>
-      {message.error ? (
-        <Alert severity="error">{message.error}</Alert>
-      ) : (
-        <Alert severity="success">{message.success}</Alert>
-      )}
-    </div>
-  );
+    const postValidatEmail = async (token) => {
+        axios
+            .post("http://localhost:3000/api/user/verifyEmail", { token })
+            .then((res) => {
+                // setMessage({ ...message, success: res.data.message });
+                console.log(res);
+            })
+            .catch((err) => {
+                // console.log(err.response.data);
+                // setMessage({ ...message, error: err.response.data.message });
+                console.log(err);
+            });
+    };
+    useEffect(() => {
+        const token = searchParams.get("token");
+        if (token) postValidatEmail(token);
+    }, []);
+    return (
+        <div>
+            {message.error ? (
+                <Alert severity="error">{message.error}</Alert>
+            ) : (
+                <Alert severity="success">{message.success}</Alert>
+            )}
+        </div>
+    );
 };
 
 export default ComfirmAccount;
