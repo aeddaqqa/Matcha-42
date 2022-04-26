f07dd7b5a87b11ffcac67cab21f74215834cb093
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-04-26 01:50:39 +0000

    edit validateCompleteProfill

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ if (!fs.existsSync(dir)) {
     });
 }
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}))
 app.use(express.static("public"));
 
 app.use("/api", user);
